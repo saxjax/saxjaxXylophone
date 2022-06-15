91e6e6f73c9890d274c932bef4bab87880d8218c
saxjax <saxjax@saxjax.dk>
2022-06-15 16:53:00 +0200

    Initial Commit

---
+++ Shared/saxjaxXylophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  saxjaxXylophoneApp.swift
+//  Shared
+//
+//  Created by Jakob Skov Søndergård on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct saxjaxXylophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
